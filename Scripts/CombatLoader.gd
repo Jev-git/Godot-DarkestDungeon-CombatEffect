48893e2934cccb2de1a154c099135e7d6add9bb2
KT-VIETNAM\tkv-nguyendinhtuan <nguyendinhtuan@koeitecmo.vn>
2021-11-01 10:24:48 +0700

    + Add assets for BlueWitch

--- Scripts/CombatLoader.gd
+++ Scripts/CombatLoader.gd
@@ -1,18 +1,25 @@
 extends Node2D
 
-export var m_psUnit: PackedScene
+var m_psUnit: PackedScene = preload("res://Scenes/PlayerUnits/BlueWitch.tscn")
+
+const PLAYER_UNIT_SCENES = [
+	preload("res://Scenes/PlayerUnits/BlueWitch.tscn"),
+	preload("res://Scenes/PlayerUnits/BlueWitch.tscn"),
+	preload("res://Scenes/PlayerUnits/BlueWitch.tscn"),
+	preload("res://Scenes/PlayerUnits/BlueWitch.tscn")
+]
 
 onready var m_nPlayerUnits: Node2D = get_tree().get_nodes_in_group("PlayerUnits")[0]
 onready var m_nEnemyUnits: Node2D = get_tree().get_nodes_in_group("EnemyUnits")[0]
 
 func _ready():
 	for iUnit in range(4):
-		var nPlayerUnit: Unit = m_psUnit.instance()
+		var nPlayerUnit: Unit = PLAYER_UNIT_SCENES[iUnit].instance()
+		nPlayerUnit.position.x = -iUnit * VISUAL_CONSTS.DISTANCE_BETWEEN_UNIT
+		m_nPlayerUnits.add_child(nPlayerUnit)
+	
+	for iUnit in range(4):
 		var nEnemyUnit: Unit = m_psUnit.instance()
-		
-		nPlayerUnit.position.x = - iUnit * VISUAL_CONSTS.DISTANCE_BETWEEN_UNIT
 		nEnemyUnit.position.x = iUnit * VISUAL_CONSTS.DISTANCE_BETWEEN_UNIT
 		nEnemyUnit.scale.x = -1
-		
-		m_nPlayerUnits.add_child(nPlayerUnit)
 		m_nEnemyUnits.add_child(nEnemyUnit)
